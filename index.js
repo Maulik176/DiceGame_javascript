4c3940cf6578f223970d0219cc76da56e28ed787
Maulik Ranadive <maulikranadive176@Mauliks-MacBook-Air.local>
2023-12-25 12:15:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var randomNumber = Math.floor( (Math.random()*6) + 1);
+
+var randomNumber2 = Math.floor( (Math.random()*6) + 1);
+
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber + ".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+if(randomNumber > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!! 🙌";
+}
+else if(randomNumber < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!! 🙌";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!!";
+}
+
